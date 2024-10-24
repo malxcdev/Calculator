ac7c03cd7e95c84db16a297446f7f7a1c0cc3e5c
Jefferson Malxcon Tito Tairo <malxcondev@gmail.com>
2024-10-23 23:17:37 -0500

    Add Funcion de suma, resta, division y multiplicacion

--- script.js
+++ script.js
@@ -1,5 +1,68 @@
-let button = document.getElementById('button')
+//  Declaramos variables para elementos del DOM
 
-button.addEventListener('click', () =>{
-    console.log("Hiciste Click")
-})
+
+// Input's
+
+let num1 = document.getElementById('num1')
+let num2 = document.getElementById('num2')
+
+//Botones 
+
+let btnSumar = document.getElementById('sumar')
+let btnRestar = document.getElementById('restar')
+let btnMultiplicar = document.getElementById('multiplicar')
+let btnDividir = document.getElementById('dividir')
+
+//resultado
+
+let resultado = document.getElementById('resultado')
+
+// Funciones 
+
+function sumar() {
+    let numero1 = parseFloat(num1.value);
+    let numero2 = parseFloat(num2.value);
+
+    let resultante = numero1 + numero2;
+    console.log(resultante);
+
+    resultado.innerText = "El Resultado es: " + resultante;
+}
+
+function restar() {
+    let numero1 = parseFloat(num1.value);
+    let numero2 = parseFloat(num2.value);
+
+    let resultante = numero1 - numero2;
+    console.log(resultante);
+
+    resultado.innerText = "El Resultado es: " + resultante;
+}
+
+function multiplicar() {
+    let numero1 = parseFloat(num1.value);
+    let numero2 = parseFloat(num2.value);
+
+    let resultante = numero1 * numero2;
+    console.log(resultante);
+
+    resultado.innerText = "El Resultado es: " + resultante;
+}
+
+function dividir() {
+    let numero1 = parseFloat(num1.value);
+    let numero2 = parseFloat(num2.value);
+
+    let resultante = numero1 / numero2;
+    console.log(resultante);
+
+    resultado.innerText = "El Resultado es: " + resultante;
+}
+
+
+//LLamado de cada click
+
+btnSumar.addEventListener('click',sumar)
+btnRestar.addEventListener('click',restar)
+btnMultiplicar.addEventListener('click',multiplicar)
+btnDividir.addEventListener('click',dividir)
